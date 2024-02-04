6e9e61f2210a37ec81fde45df603f1e0bb881d5e
Rohit Bhadane <rohitbhadane310@gmail.com>
2024-02-04 10:15:47 +0530

    small update

--- src/layouts/ReviewCard.jsx
+++ src/layouts/ReviewCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ReviewCard = (props) => {
+const ReviewCard = ({ img, name }) => {
   return (
     <div className=" bg-white border-2 border-lightText md:border-none p-5 rounded-lg shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
       <div>
@@ -13,8 +13,8 @@ const ReviewCard = (props) => {
       </div>
 
       <div className=" flex flex-row justify-center items-center mt-4 gap-4">
-        <img className=" rounded-full w-1/4" src={props.img} alt="img" />
-        <h3 className=" font-semibold ">{props.name}</h3>
+        <img className=" rounded-full w-1/4" src={img} alt="img" />
+        <h3 className=" font-semibold ">{name}</h3>
       </div>
     </div>
   );
